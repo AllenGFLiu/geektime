f83feda281af54fdc3e6a77dbbcec392751290b6
H7215518 <h7215518@dpbg.lh.com>
2019-10-17 10:03:56 +0800

    新增图的广度优先搜索代码实现

---
+++ graph/bfs_dfs.py
@@ -0,0 +1,59 @@
+# -*-: coding:utf-8 -*-
+from collections import deque
+from graph import UndirectedGraph
+
+
+def print_path(fr, to, prev):
+    '''yield from Iterable = for value in Iterable : yield value 
+    '''
+    if prev[fr] or fr != to:
+        yield from print_path(fr, prev[to], prev)
+    yield str(to)
+
+
+def bfs(graph_object, fr, to):
+    '''base on UndirectedGraph
+    find the path from Vertext fr to Vertex to,using bfs
+    visited:用来记录已经顶点是否已经被访问
+    queue:用来存储已经被访问，但相连的顶点还没有被访问的顶点
+    prev:用来记录搜索路径，不过是反向存储的，prev[3]=2就是说从顶点2到达的顶点3，
+        所以需要使用递归来打印所有路径。
+    '''
+    if fr == to: return
+    visited = [False] * graph_object.v_number
+    visited[fr] = True
+
+    q = deque()
+    q.append(fr)
+    prev = [None] * graph_object.v_number
+
+    while q:
+        v = q.popleft()
+        for related in graph_object[v]:
+            if not visited[related]:
+                prev[related] = v
+                if related == to:
+                    print('->'.join(print_path(fr, to, prev)))
+                    return
+                visited[related] = True
+                q.append(related)
+
+
+if __name__ == '__main__':
+    ug = UndirectedGraph(8)
+    ug.add_edge(0, 1)
+    ug.add_edge(0, 3)
+    ug.add_edge(1, 2)
+    ug.add_edge(1, 4)
+    ug.add_edge(2, 5)
+    ug.add_edge(3, 4)
+    ug.add_edge(4, 5)
+    ug.add_edge(4, 6)
+    ug.add_edge(5, 7)
+    ug.add_edge(6, 7)
+    bfs(ug, 0, 7)
+
+
+
+
+
---
+++ graph/graph.py
@@ -0,0 +1,54 @@
+# -*- coding:utf-8 -*-
+
+
+class UndirectedGraph:
+    """无向图的实现
+    """
+    def __init__(self, vertex_number):
+        self.v_number = vertex_number
+        self.adj_table = [[] for _ in range(self.v_number)]
+
+    def add_edge(self, s, t):
+        '''无向图里建立边的关系需要保存对向的两个顶点的数据
+        '''
+        if s > self.v_number or t > self.v_number:
+            return False
+        self.adj_table[s].append(t)
+        self.adj_table[t].append(s)
+        return True
+
+    def __getitem__(self, index):
+        return self.adj_table[index]
+
+    def __repr__(self):
+        return str(self.adj_table)
+
+
+class DirectedGraph:
+    """有向图的实现
+    """
+    def __init__(self, vertex_number):
+        self.v_number = vertex_number
+        self.adj_table = [[] for _ in range(self.v_number)]
+
+    def add_edge(self, fr, to):
+        '''有向图中只保存本顶点指向的顶点信息
+        '''
+        if fr > self.v_number or to > self.v_number:
+            return False
+        self.adj_table[fr].append(to)
+        return True
+
+    def __repr__(self):
+        return str(self.adj_table)
+
+if __name__ == '__main__':
+    ug = UndirectedGraph(3)
+    ug.add_edge(0, 1)
+    ug.add_edge(0, 2)
+    print(ug)
+
+    dg = DirectedGraph(3)
+    dg.add_edge(0, 1)
+    dg.add_edge(0, 2)
+    print(dg)
