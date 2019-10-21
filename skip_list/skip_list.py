6b18b4f56a16da4e2239216b9c46f08eeaa3a736
H7215518 <h7215518@dpbg.lh.com>
2019-10-21 11:21:19 +0800

    为跳表新增查找功能代码实现

--- skip_list/skip_list.py
+++ skip_list/skip_list.py
@@ -1,4 +1,18 @@
 # -*- coding:utf-8 -*-
+# 在单链表之上增加多层索引的数据结构就是跳表
+# 跳表是动态数据结构，查找/插入/删除的时间复杂度都是O(logn), 空间复杂度是O(n),是以空间换时间的设计理念
+# 跳表的节点在插入时通过random的形式确定插入调表后拥有的层数，具体看insert方法的注释
+# 跳表示例图：
+# | |-------->| |-------->
+# | |-------->| |-------->
+# | |-------->| |-------->
+# | |-------->|6|-------->
+# | |-->| |-->| |-------->
+# | |-->|3|-->| |-->|7|-->之后的省略
+# 其中第一个节点是self._head节点，定义为一个有MAX_LEVEL层级的空节点
+# 第二个节点3有2层 第三个节点6有5层 第四个节点7有1层 通过这几个举例应该就能看懂insert方法了
+# Author: AllenGFLiu
+# #####################################################
 import random
 
 
@@ -9,8 +23,8 @@ class ListNode:
 
 
 class SkipList:
-
-    MAX_LEVEL = 16
+    # 此样例代码只存储整数，并且不重复
+    MAX_LEVEL = 16  # 定义一个最大层级数
 
     def __init__(self):
         self._level_count = 1
@@ -24,7 +38,7 @@ class SkipList:
         return level
 
     def insert(self, value):
-        level = self.random_level()
+        level = self.random_level()  # 以random的形式去随机得到新节点插入调表时所拥有的层数(包含数据层和索引层)
         if self._level_count < level:
             self._level_count = level
         
@@ -44,12 +58,25 @@ class SkipList:
             update[i]._forwards[i] = new_node
 
     def __repr__(self):
+        # 此方法只返回了本聊表的原始链表层数据
+        # 不包含在此链表层之上建立的索引层数据
         values = []
         p = self._head
         while p._forwards[0]:
             values.append(str(p._forwards[0]._data))
             p = p._forwards[0]
         return '->'.join(values)
+
+    def find(self, value):
+        p = self._head
+        for i in range(self._level_count-1, -1, -1):
+            while p._forwards[i] and p._forwards[i]._data < value:
+                p = p._forwards[i]
+        
+        return p._forwrads[0] if p._forwards[0] and p._forwards[0]._data == value else None
+
+
+
 
 if __name__ == '__main__':
     l = SkipList()
